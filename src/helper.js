5c11d16a0f42abd3a6166dbe64323af761964d8e
Melena Suliteanu <melenasuliteanu@gmail.com>
2018-01-22 15:35:34 -0700

    Reduces raw data in helper file via clean data method returns an object with no duplicates

--- src/helper.js
+++ src/helper.js
@@ -1,3 +1,22 @@
 export default class DistrictRepository {
+  constructor(data) {
+    this.data = this.cleanData(data);
+  }
 
+  cleanData(data) {
+    return data.reduce( (cleanedData, dataPoint) => {
+      if(!cleanedData[dataPoint.Location]) {
+        cleanedData[dataPoint.Location] = {};
+      }
+
+      return cleanedData;
+    }, {})
+  }
 }
+
+
+// Colorado: {
+//   Location: Colorado,
+//   2005: 0.3456245,
+//   2006:
+// }
