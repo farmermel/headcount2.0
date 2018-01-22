d290a96e8a23191cb549b07277ef4da9b7022c14
Matthew Renn <rennmatthewp@gmail.com>
2018-01-22 16:35:01 -0700

    refactors imperitive if statement in cleanData to declarative object creation

--- src/helper.js
+++ src/helper.js
@@ -4,36 +4,24 @@ export default class DistrictRepository {
   }
 
   cleanData(data) {
-    const woot = data.reduce( (cleanedData, dataPoint) => {
+    return data.reduce((cleanedData, dataPoint) => {
       const location = dataPoint.Location.toUpperCase();
-      if(!cleanedData[location]) {
-        cleanedData[location] = {};
-      }
 
-      cleanedData[location].location = location;
+      cleanedData[location] = {
+        ...cleanedData[location],
+        location: location
+      };
 
       cleanedData[location].data = {
         ...cleanedData[location].data,
         [dataPoint.TimeFrame]: Math.round(dataPoint.Data * 1000) / 1000 || 0
       };
-      
+
       return cleanedData;
-    }, {}) 
-    return woot
-    
+    }, {});
   }
 
   findByName(input = '') {
-    input = input.toUpperCase();
-    return this.data[input]
+    return this.data[input.toUpperCase()];
   }
 }
-
-
-// Colorado: {
-//   Location: Colorado,
-//   data: {
-  //   2005: 0.3456245,
-  //   2006: 0
-  // }
-//}
