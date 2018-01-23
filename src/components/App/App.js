292c6ce9deb17632ad1e44c3b4b3369ee3686d2b
Matthew Renn <rennmatthewp@gmail.com>
2018-01-22 18:19:14 -0700

    moves data directory into src directory to satisfy our reacty overlords

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
+import CardContainer from "../CardContainer/CardContainer";
+import DistrictRepository from "../../helper";
+import kinderData from '../../data/kindergartners_in_full_day_program';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      districts: []
+    }
+  }
+
+  componentDidMount() {
+    const districts = new DistrictRepository(kinderData).findAllMatches();
+    this.setState({districts})
+  }
+  
   render() {
     return (
-      <div>Welcome To Headcount 2.0</div>
+      <div>
+        <CardContainer districts={this.state.districts}/>
+      </div>
     );
   }
 }
