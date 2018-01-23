73a15e91451b18f3f6f6faf1abb5a1ab1be68cb7
Matthew Renn <rennmatthewp@gmail.com>
2018-01-23 13:09:29 -0700

    reverses last commit

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import CardContainer from '../CardContainer/CardContainer';
 import DistrictRepository from '../../helper';
-import kinderData from '../../../data/kindergartners_in_full_day_program';
+import kinderData from '../../data/kindergartners_in_full_day_program';
 import './App.css';
 
 class App extends Component {
