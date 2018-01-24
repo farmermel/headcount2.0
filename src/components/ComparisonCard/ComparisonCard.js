daaed611f0475d7f6dc3727789c5ae5305bc3c02
Matthew Renn <rennmatthewp@gmail.com>
2018-01-24 15:26:51 -0700

    adds initial ComparisonCard component

---
+++ src/components/ComparisonCard/ComparisonCard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ComparisonCard = () => {
+  return (
+    <div className="card">
+      Click on a couple districts to compare them!
+    </div>
+  );
+};
+
+export default ComparisonCard;
