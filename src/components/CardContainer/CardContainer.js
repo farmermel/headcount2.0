755f4016866661bcb51a98f6a67570cadce52abc
Melena Suliteanu <melenasuliteanu@gmail.com>
2018-01-24 15:54:37 -0700

    Take compare function as props and pass to card

--- src/components/CardContainer/CardContainer.js
+++ src/components/CardContainer/CardContainer.js
@@ -3,9 +3,10 @@ import Card from '../Card/Card';
 import PropTypes from 'prop-types';
 import './CardContainer.css';
 
-const CardContainer = ({ districts }) => {
+const CardContainer = ({ districts, compare }) => {
   const allDistricts = districts.map(district => (
-    <Card {...district} key={district.location} />
+    <Card {...district} key={district.location}
+                        compare={compare} />
   ));
 
   return <div className="card-container">{allDistricts}</div>;
