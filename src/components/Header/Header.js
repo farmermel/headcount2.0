5cbcda9f991aebda433676737be99d5a604a84a8
Matthew Renn <rennmatthewp@gmail.com>
2018-01-24 11:07:08 -0700

    imports Search component passes searchDistrcts method down to Search

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Search from "../Search/Search";
 import './Header.css';
 
-const Header = () => {
+const Header = ({searchDistricts}) => {
   return (
     <header>
       <h1>HeadCount2.0</h1>
+      <Search searchDistricts={searchDistricts} />
     </header>
   );
 };
