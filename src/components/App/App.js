b33159e8dfd8bc7bf1ec6102192516e4f45f0a0b
Matthew Renn <rennmatthewp@gmail.com>
2018-01-24 11:06:19 -0700

    adds searchDistricts method to App component returns districts that match the search query passes searchDistricts as props to Header

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import CardContainer from '../CardContainer/CardContainer';
-import Header from "../Header/Header";
+import Header from '../Header/Header';
 import DistrictRepository from '../../helper';
 import kinderData from '../../data/kindergartners_in_full_day_program';
 import './App.css';
@@ -9,20 +9,28 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      districts: []
+      districts: [],
+      allDistricts: {}
     };
   }
 
   componentDidMount() {
-    const districts = new DistrictRepository(kinderData).findAllMatches();
-    
-    this.setState({ districts });
+    const allDistricts = new DistrictRepository(kinderData);
+    const districts = allDistricts.findAllMatches();
+
+    this.setState({ districts, allDistricts });
   }
+
+  searchDistricts = input => {
+    const districts = this.state.allDistricts.findAllMatches(input);
+
+    this.setState({ districts });
+  };
 
   render() {
     return (
       <div>
-        <Header />
+        <Header searchDistricts={this.searchDistricts} />
         <CardContainer districts={this.state.districts} />
       </div>
     );
