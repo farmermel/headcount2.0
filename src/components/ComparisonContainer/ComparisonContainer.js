ab7739f6d995819fa92fe3899eef2511308884f3
Matthew Renn <rennmatthewp@gmail.com>
2018-01-24 15:27:17 -0700

    adds initial ComparisonContainer component

---
+++ src/components/ComparisonContainer/ComparisonContainer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ComparisonCard from '../ComparisonCard/ComparisonCard';
+
+const ComparisonContainer = props => {
+  return (
+    <div className="card-container">
+      <ComparisonCard />
+    </div>
+  );
+};
+
+export default ComparisonContainer;
