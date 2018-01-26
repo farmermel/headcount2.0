2eca1b59311a148d38420a64dca637cbc90a40d9
Melena Suliteanu <melenasuliteanu@gmail.com>
2018-01-25 17:17:38 -0700

    Pass props to card container and display selected cards

--- src/components/ComparisonContainer/ComparisonContainer.js
+++ src/components/ComparisonContainer/ComparisonContainer.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import Card from '../Card/Card';
 import ComparisonCard from '../ComparisonCard/ComparisonCard';
 
-const ComparisonContainer = props => {
+const ComparisonContainer = ({cards, compare}) => {
+  const selectedDistricts = cards.map( district => {
+    return <Card {...district} key={district.location}
+                               compare={compare} />
+  })
   return (
     <div className="card-container">
+      {selectedDistricts[0]}
       <ComparisonCard />
+      {selectedDistricts[1]}
     </div>
   );
 };
