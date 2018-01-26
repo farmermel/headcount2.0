2b7fea87de103516042424d08961658edbc790ce
Melena Suliteanu <melenasuliteanu@gmail.com>
2018-01-26 16:51:49 -0700

    Make mock districtRepository with mock functions that return simplified versions of data

--- src/__mocks__/helper.js
+++ src/__mocks__/helper.js
@@ -1,8 +1,51 @@
 // Import this named export into your test file:
 export const mockDistrictRepository = jest.fn();
 
+const districtsArray = [
+  {
+    location: 'COLORADO',
+    data: {
+      2003: 0.4,
+      2004: 0.8
+    }
+  },
+  {
+    location: 'ASPEN 20',
+    data: {
+      2003: 0.7,
+      2004: 0.9
+    }
+  }
+]
+
+// function __findAllMatches(input) {
+//   if (!input) {
+//     return districtsArray;
+//   }
+
+//   return districtsArray.filter(district => {
+//     return district.location.includes(input.toUpperCase());
+//   });
+// }
+
+const __findAllMatches = jest.fn().mockImplementation((input) => {
+  if (!input) {
+    return districtsArray;
+  }
+
+  return districtsArray.filter(district => {
+    return district.location.includes(input.toUpperCase());
+  });
+})
+
+const __findByName = jest.fn().mockImplementation((input) => {
+  return districtsArray[input.toUpperCase()];
+})
+
 const mock = jest.fn().mockImplementation(() => {
-  return { DistrictRepository: mockDistrictRepository };
+  return { findAllMatches: __findAllMatches,
+           findByName: __findByName,
+           data: districtsArray };
 });
 
 export default mock;
