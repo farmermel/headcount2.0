15da937ab7693a6c6ec50d7150dae9f412770592
Melena Suliteanu <melenasuliteanu@gmail.com>
2018-01-26 16:57:50 -0700

    Add image to header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import Search from "../Search/Search";
+import check from "../../images/sign.svg"
 import './Header.css';
 
 const Header = ({searchDistricts}) => {
   return (
     <header>
-      <h1>HeadCount2.0</h1>
+      <div className="header-wrap">
+        <img src={check}
+             alt="check"
+             className="check-icon"/>
+        <h1 className="title">HeadCount2.0</h1>
+      </div>
       <Search searchDistricts={searchDistricts} />
     </header>
   );
