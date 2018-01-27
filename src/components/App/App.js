97f26f8e1d4100d56eeb38ff8b96884452b8bbb0
Matthew Renn <rennmatthewp@gmail.com>
2018-01-27 09:32:37 -0700

    removes commented-out code changes naming for compare object

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,14 +6,13 @@ import DistrictRepository from '../../helper';
 import kinderData from '../../data/kindergartners_in_full_day_program';
 import './App.css';
 
-
 class App extends Component {
   constructor() {
     super();
     this.state = {
       districts: [],
       districtRepository: {},
-      compare: {}
+      comparison: {}
     };
   }
 
@@ -31,51 +30,42 @@ class App extends Component {
   };
 
   removeDuplicates(district) {
-    let compare = this.state.compare;
-    let location = district.location
+    let comparison = this.state.comparison;
+    let location = district.location;
 
-    compare[location] 
-      ?  delete compare[location]
-      : compare[location] = district;
+    comparison[location]
+      ? delete comparison[location]
+      : (comparison[location] = district);
 
-    return compare;
+    return comparison;
   }
 
   toggleCompare(district) {
-    let compare = this.removeDuplicates(district)
-    let keys = Object.keys(compare)
+    let comparison = this.removeDuplicates(district);
+    let keys = Object.keys(comparison);
 
-    keys.length > 2 && delete compare[keys[1]]
+    keys.length > 2 && delete comparison[keys[1]];
 
-    this.setState({ compare })
+    this.setState({ comparison });
   }
 
   handleClick = district => {
-    let compare = this.state.compare;
+    let comparison = this.state.comparison;
 
     district = this.state.districtRepository.findByName(district);
-    this.toggleCompare(district)
+    this.toggleCompare(district);
   };
-
-
-
-  // removefirstCard = compareArray => {
-  //   let firstCard = compareArray.shift();
-  //   firstCard.selected = false;
-  //   const districts = this.state.districts.map(district => {
-  //     return district.location === firstCard.location
-  //       ? (district = firstCard)
-  //       : district;
-  //   });
-  //   this.setState({ compare: compareArray });
-  // };
 
   render() {
     return (
       <div>
         <Header searchDistricts={this.searchDistricts} />
-        <ComparisonContainer cards={this.state.compare} compare={this.handleClick} />
+        <ComparisonContainer
+          districts={this.state.comparison}
+          compare={this.handleClick}
+        />
         <CardContainer
+          comparison={this.state.comparison}
           districts={this.state.districts}
           compare={this.handleClick}
         />
@@ -83,92 +73,5 @@ class App extends Component {
     );
   }
 }
-
-// class App extends Component {
-//   constructor() {
-//     super();
-//     this.state = {
-//       districts: [],
-//       districtRepository: {},
-//       compare: []
-//     };
-//   }
-
-//   componentDidMount() {
-//     const districtRepository = new DistrictRepository(kinderData);
-//     const districts = districtRepository.findAllMatches();
-
-//     this.setState({ districts, districtRepository });
-//   }
-
-//   searchDistricts = input => {
-//     const districts = this.state.districtRepository.findAllMatches(input);
-
-//     this.setState({ districts });
-//   };
-
-//   toggleCompare(boolean) {
-//     compare = [...compare, district];
-//     compare.length > 2 && compare.shift();
-//     this.setState({ compare })
-//   }
-
-//   checkDuplicates(district) {
-//     let compare = this.state.compare;
-
-//     return compare.find( dist => district !== dist)
-
-//     //loop through compare array
-//     //if district is already in array, take it out and set state
-//     //somehow don't run rest of handle click if it's true???
-//   }
-
-//   handleClick = district => {
-//     let compare = this.state.compare;
-
-//     district = this.state.districtRepository.findByName(district);
-//     this.toggleCompare(this.checkDuplicates(district))
-//     // compare = [...compare, district];
-//     // compare.length > 2 && compare.shift();
-//     // this.setState({ compare })
-
-
-//     // //if a card has been clicked before AND there are two cards
-//     // //in compare, the card doesn't go into compare
-//     // const toCompare = this.state.districtRepository.findByName(district);
-//     // toCompare.selected = !toCompare.selected;
-//     // let compare = this.state.districts.filter(district => district.selected);
-//     // // compare.length > 2
-//     // //   ? this.removefirstCard(compare)
-//     // //   : this.setState({ compare });
-
-//   };
-
-
-
-//   removefirstCard = compareArray => {
-//     let firstCard = compareArray.shift();
-//     firstCard.selected = false;
-//     const districts = this.state.districts.map(district => {
-//       return district.location === firstCard.location
-//         ? (district = firstCard)
-//         : district;
-//     });
-//     this.setState({ compare: compareArray });
-//   };
-
-//   render() {
-//     return (
-//       <div>
-//         <Header searchDistricts={this.searchDistricts} />
-//         <ComparisonContainer cards={this.state.compare} compare={this.handleClick} />
-//         <CardContainer
-//           districts={this.state.districts}
-//           compare={this.handleClick}
-//         />
-//       </div>
-//     );
-//   }
-// }
 
 export default App;
