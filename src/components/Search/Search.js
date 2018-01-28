8eb6651c26cefaec6e8bd7257b3fb65cba1cd5e5
Matthew Renn <rennmatthewp@gmail.com>
2018-01-28 13:58:27 -0700

    adds proptypes to Search

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,15 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './Search.css';
 
-const Search = ({searchDistricts}) => {
+const Search = ({ searchDistricts }) => {
   return (
     <div className="search-wrap">
-      <input type="text"
-             placeholder="Search Districts"
-             className="search"
-             onChange={(e) => searchDistricts(e.target.value)} />
+      <input
+        type="text"
+        placeholder="Search Districts"
+        className="search"
+        onChange={e => searchDistricts(e.target.value)}
+      />
     </div>
   );
+};
+
+Search.propTypes = {
+  searchDistricts: PropTypes.func.isRequired
 };
 
 export default Search;
