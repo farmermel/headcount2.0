33534f057468a2c112b0310c988af6bf3e0c3e0f
Melena Suliteanu <melenasuliteanu@gmail.com>
2018-01-28 12:28:52 -0700

    Style comparison card

--- src/components/ComparisonCard/ComparisonCard.js
+++ src/components/ComparisonCard/ComparisonCard.js
@@ -2,18 +2,24 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './ComparisonCard.css';
 
-const ComparisonCard = ({ districts, comparativeAnalysis}) => {    
+const ComparisonCard = ({ districts, comparativeAnalysis }) => {    
   const district1key = Object.keys(districts)[0];
   const district2key = Object.keys(districts)[1];
-  const district1 = district1key ? `: ${districts[district1key].avg}` : 'Pick first card to compare';
-  const district2 = district2key ? `: ${districts[district2key].avg}` : 'Pick second card to compare';
+  const district1 = district1key ? districts[district1key].avg : 'Pick first card to compare';
+  const district2 = district2key ? districts[district2key].avg : 'Pick second card to compare';
   const analysis = comparativeAnalysis.compared ? comparativeAnalysis.compared : ''
   
   return (
     <div className="card comparison-card">
-      <h3>{district1key} {district1}</h3>
-      <h3>{analysis}</h3>
-      <h3>{district2key} {district2}</h3>
+      <div>
+        <h3>{district1key}</h3>
+        <p>{district1}</p>
+      </div>
+      <h3 className="ratio">{analysis}</h3>
+            <div>
+      <h3>{district2key}</h3>
+      <p>{district2}</p>
+      </div>
     </div>
   );
 };
