597d36f43b666300d4bd586ee003c33cd73984db
Matthew Renn <rennmatthewp@gmail.com>
2018-01-28 13:14:10 -0700

    adds propTypes and prettify

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,20 +1,23 @@
 import React from 'react';
-import Search from "../Search/Search";
-import check from "../../images/sign.svg"
+import Search from '../Search/Search';
+import check from '../../images/sign.svg';
+import PropTypes from 'prop-types';
 import './Header.css';
 
-const Header = ({searchDistricts}) => {
+const Header = ({ searchDistricts }) => {
   return (
     <header>
       <div className="header-wrap">
-        <img src={check}
-             alt="check"
-             className="check-icon"/>
+        <img src={check} alt="check" className="check-icon" />
         <h1 className="title">HeadCount2.0</h1>
       </div>
       <Search searchDistricts={searchDistricts} />
     </header>
   );
 };
+
+Header.propTypes = {
+  searchDistricts: PropTypes.func.isRequired
+}
 
 export default Header;
